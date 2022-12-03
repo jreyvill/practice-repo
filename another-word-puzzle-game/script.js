406a869844ac4c65d9b15922da692d079efdd4f4
jreyvill <johnrey.villademosa@studytafensw.edu.au>
2022-12-03 15:39:39 +1100

    update

--- another-word-puzzle-game/script.js
+++ another-word-puzzle-game/script.js
@@ -1,7 +1,6 @@
 "use strict"
 const MAX_ANSWER_LENGTH = 5;
 const allLetters = document.querySelectorAll('.letter') // getting all the letters
-console.log(allLetters)
 const loadingDiv = document.querySelector('info-bar') //loading indicator
 
 
@@ -25,18 +24,43 @@ async function init() {
         } 
     });
 
+    /***
+     * Function responsible for placing letter in the square box
+     */
     let currentGuess = '';
+    let currentRow = 0;
     function addLetter(letter) {
 
         if (currentGuess.length < MAX_ANSWER_LENGTH) {
+            
             currentGuess += letter;
         } else {
             currentGuess  = currentGuess.substring(0, currentGuess.length - 1) + letter; // replace the last etter
         }
         
-        allLetters[currentGuess.length - 1].innerText = letter;
+        // determine which row to place letter
+        allLetters[MAX_ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
     }
 
+    /***
+     * Function responsible for handling if validaiton of the word, win or lose, correct/close/wrong word
+     */
+    async function commitGuess() {
+        // check if maximum letter length of guess is 5 letters
+        if (currentGuess.length !== MAX_ANSWER_LENGTH) {
+            //do nothing, exit funtion
+            return;
+        }
+
+        // TODO validate the word
+
+        // TODO do all the marking as "correct" "close" or "wrong"
+
+        // TODO did they win or lose?
+        
+        currentRow++;
+        currentGuess = '';
+    }
   
 }
 
